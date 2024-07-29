95e76cfbd8bf8d6b77ca815f8defaf27287cbf13
Shilpi <mailtosjohri@gmail.com>
2024-07-29 07:23:31 -0500

    int to byte primitive typecasting

---
+++ Primitive_typecasting_Int_to_Byte.java
@@ -0,0 +1,12 @@
+package collection;
+
+public class Primitive_typecasting_Int_to_Byte {
+
+	public static void main(String[] args) {
+		int a=87001;
+		byte b=(byte) a;
+		System.out.println(a);
+		System.out.println(b);
+	}
+
+}
