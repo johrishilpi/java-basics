fef5e9a9de26be6729efcd030d5a3fdb7af6c0cc
Shilpi <mailtosjohri@gmail.com>
2024-07-29 07:28:08 -0500

    Double to float Narrowing

---
+++ Double_Float_narrowing.java
@@ -0,0 +1,12 @@
+package collection;
+
+public class Double_Float_narrowing {
+public static void main(String[] args) {
+	double b=1.78987777777;
+	float a=(float) b;
+	System.out.println(a);
+	float c=(float) 1.67899788989889;
+	System.out.println(c);
+}
+	
+}
