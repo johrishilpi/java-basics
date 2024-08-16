96b1f56f323f7330e43616c202e5837d548ca891
Shilpi <mailtosjohri@gmail.com>
2024-08-15 19:39:32 -0500

    Hybrid level inheritance

---
+++ assign/RakeshJohri.java
@@ -0,0 +1,20 @@
+package hybrid.inheritance.assign;
+
+public class RakeshJohri extends Raghunath
+{
+	 static void rachna()
+	{
+		System.out.println("Rachna");
+	}
+
+	public static void main(String[] args) 
+	{
+	System.out.println("Rakesh Johri");
+	rachna();
+	sushila();
+	
+	
+
+	}
+
+}
---
+++ assign/Shilpi.java
@@ -0,0 +1,39 @@
+package hybrid.inheritance.assign;
+class Raghunath
+{
+	static void sushila()
+	{
+		System.out.println("Raghunath Sharan Johri");
+	}
+}
+class Vineet extends Raghunath
+{
+	static void vandana()
+	{
+		System.out.println("Vineet Johri");
+	}
+	
+}
+
+public class Shilpi extends Vineet
+{
+	static void saurabh()
+	{
+		System.out.println("Saurabh Johri");
+	}
+	
+	
+public static void main(String[] args) 
+      {
+	System.out.println("Shilpi johri");
+	saurabh();
+	vandana();
+	sushila();
+	RakeshJohri.rachna();
+	RakeshJohri.sushila();
+	
+	
+	
+	
+      }
+}
