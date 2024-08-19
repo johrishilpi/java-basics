baafccf686bb256455742db6bed432926a98e6ea
Shilpi <mailtosjohri@gmail.com>
2024-08-18 23:08:19 -0500

    call constructor

---
+++ Constructor.java
@@ -0,0 +1,15 @@
+package assignments;
+
+public class Constructor {
+	
+	Constructor()
+	{
+		System.out.println("Constructor-Non Parametrized");
+	}
+	
+	public static void main(String[] args) {
+		new Constructor();
+
+	}
+
+}
