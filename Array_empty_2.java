e06f8e7db602abe06a033ba9dcc01df3076507d1
Shilpi <mailtosjohri@gmail.com>
2024-08-25 17:22:13 -0500

    check array elements with no value

---
+++ Array_empty_2.java
@@ -0,0 +1,20 @@
+package java_programs;
+
+import java.util.Arrays;
+
+public class Array_empty_2 {
+
+	public static void main(String[] args) {
+	int [] a1=new int[5];
+	a1[0]=3;
+	a1[1]=6;
+	a1[2]=8;
+    System.out.println(Arrays.toString(a1));
+    String [] a2=new String[5];
+    a2[0]="Shilpi";
+    a2[1]="Sanjivani";
+    a2[2]="Ashwini";
+    System.out.println(Arrays.toString(a2));
+	}
+
+}
